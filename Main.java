8c40fdbb927e75cd6479e92c211308c512a4449b
Dimitar84 <46647887+Dimitar84@users.noreply.github.com>
2023-07-24 00:09:38 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -55,43 +55,45 @@ public class Main {
 
 
 
-        public static String convertToRomeNumbers(int num) {
-            String romeNumber = null;
-            switch (num) {
-                case 1:
-                    romeNumber = "I";
-                    break;
-                case 2:
-                    romeNumber = "II";
-                    break;
-                case 3:
-                    romeNumber = "III";
-                    break;
-                case 4:
-                    romeNumber = "IV";
-                    break;
-                case 5:
-                    romeNumber = "V";
-                    break;
-                case 6:
-                    romeNumber = "VI";
-                    break;
-                case 7:
-                    romeNumber = "VII";
-                    break;
-                case 8:
-                    romeNumber = "VIII";
-                    break;
-                case 9:
-                    romeNumber = "IX";
-                    break;
-                case 10:
-                    romeNumber = "X";
-                    break;
-            }
-            return romeNumber;
+
+
+
+    public static String convertToRomeNumbersFull(int num) {
+        String[] romeNumber = new String[2];
+        int[] digit = new int[2];
+        digit[1] = num/10;
+        digit[0] = num%10;
+
+        switch (digit[1]){
+            case 0: romeNumber[1] = "";break;
+            case 1: romeNumber[1] = "X";break;
+            case 2: romeNumber[1] = "XX";break;
+            case 3: romeNumber[1] = "XXX";break;
+            case 4: romeNumber[1] = "XL";break;
+            case 5: romeNumber[1] = "L";break;
+            case 6: romeNumber[1] = "LX";break;
+            case 7: romeNumber[1] = "LXX";break;
+            case 8: romeNumber[1] = "LXXX";break;
+            case 9: romeNumber[1] = "XC";break;
+            case 10: romeNumber[1] = "C";break;
         }
 
+        switch (digit[0]){
+            case 1: romeNumber[0] = "I";break;
+            case 2: romeNumber[0] = "II";break;
+            case 3: romeNumber[0] = "III";break;
+            case 4: romeNumber[0] = "IV";break;
+            case 5: romeNumber[0] = "V";break;
+            case 6: romeNumber[0] = "VI";break;
+            case 7: romeNumber[0] = "VII";break;
+            case 8: romeNumber[0] = "VIII";break;
+            case 9: romeNumber[0] = "IX";break;
+            case 0: romeNumber[0] = "";break;
+        }
+
+
+        return romeNumber[1]+romeNumber[0];
+    }
 
 
         public static void main (String[]args) throws IOException {
@@ -132,7 +134,7 @@ public class Main {
                 var1 = convertToArabNumbers(array[0]);
                 var2 = convertToArabNumbers(array[2]);
 
-                if (var1 < var2) throw new IOException();
+
 
                 switch (array[1]) {
                     case "+":
@@ -149,16 +151,9 @@ public class Main {
                         break;
 
                 }
-                System.out.println(convertToRomeNumbers(result));
+                if (result < 0) throw new IOException();
+                System.out.println(convertToRomeNumbersFull(result));
             }
-
-
-
-
-
-
-
-
 
         }
     }
